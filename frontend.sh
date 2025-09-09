9be6593db3a3fec085ed7d8b95dd6eb4283e9af0
shalemraju <shalemrajudevops@gamil.com>
2025-09-09 11:18:52 +0530

    shell-ecpnse-p2

--- frontend.sh
+++ frontend.sh
@@ -55,7 +55,7 @@ VALIDATE $? "Moving to HTML directory"
 unzip /tmp/frontend.zip &>>$LOG_FILE_NAME
 VALIDATE $? "unzipping the frontend code"
 
-cp /home/ec2-userexpense-shell-p2/expense.conf /etc/nginx/default.d/expense.conf
+cp /home/ec2-user/expense-shell-p2/expense.conf /etc/nginx/default.d/expense.conf
 VALIDATE $? "Copied expense config"
 
 systemctl restart nginx &>>$LOG_FILE_NAME
